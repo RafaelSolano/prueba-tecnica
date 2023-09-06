0a54af5d430e53c00da224b1f9a3934f542b04c6
unknown <rafaelsolanol007@gmail.com>
2023-09-06 07:51:35 -0500

    Se crean los estilos iniciales de los componentes- first-mobile

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,23 +1,28 @@
 import React from 'react'
 import "./styles.css"
 export const Hero = () => {
-  return (
-    <>
-    <div className="hero">
-        <p className="hero__min_title">Our blog</p>
-        <h1 className="hero__title">Stories and interviews</h1>
-        <p className=" hero__text">Subscribe to learn about new product features, the latest in technology, solutions, and updates. </p>
+    return (
+        <>
+            <div className="hero">
+                <div className='hero__container-text'>
+                    <p className="hero__min_title">Our blog</p>
+                    <h1 className="hero__title">Stories and interviews</h1>
+                    <p className=" hero__text">Subscribe to learn about new product features, the latest in technology, solutions, and updates. </p>
 
 
-        <div className="subscribe">
-            <form className="subscribe__form"  action="">
-                <input className="subscribe__input"  type="email" name="" id="" placeholder=" Enter your email"/>
-                <button className="subscribe__btn" type="submit"> Suscribe</button>
-            </form>
-            <p className="subscribe__form-info">We care about your data in our  <a class="subscribe__link_privacy" href="#">privacy policy</a></p>
 
-        </div>
-    </div>
-    </>
-  )
+                </div>
+
+                <div className="subscribe">
+                    <form className="subscribe__form" action="">
+                        <input className="subscribe__input" type="email" name="" id="" placeholder=" Enter your email" />
+                        <p className="subscribe__form-info">We care about your data in our  <a class="subscribe__link_privacy" href="#">privacy policy</a></p>
+                        <button className="subscribe__btn" type="submit"> Subscribe</button>
+                    </form>
+                   
+
+                </div>
+            </div>
+        </>
+    )
 }
