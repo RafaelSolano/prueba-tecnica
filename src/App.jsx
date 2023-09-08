d34f8d47bc6afc7bfbdee12cae566345a33fc2c3
unknown <rafaelsolanol007@gmail.com>
2023-09-07 19:54:46 -0500

    preparando para despliegue

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { Hero } from './components/Hero'
-import { AllBlogs } from './containers/AllBlogs'
-import { RecentBlogs } from './containers/RecentBlogs/allBlogs'
+import { AllBlogs } from './containers/AllBlogs/AllBlogs'
+import { RecentBlogs } from './containers/RecentBlogs/RecentBlogs'
 
 function App() {
  
